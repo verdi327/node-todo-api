3b87ac65413b16811e5752046a05890ce9593031
Michael Verdi <michael.v.verdi@gmail.com>
2019-01-31 10:49:11 -0500

    adding mongoose and express

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,9 +1,16 @@
 const {MongoClient, ObjectID} = require("mongodb")
+const databaseUrl = "mongodb://localhost:27017/TodoApp"
 
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+MongoClient.connect(databaseUrl, (err, db) => {
 	if (err) {
 		return console.log("Unable to connect to MongoDB")
 	}
+
+	// db calls here
+
+	db.close();
+})
+
 
 	// db.collection("Todos").findOneAndUpdate(
 	// 	{ _id: new ObjectID("5c5214c1b9f45bb9fe9eec5d")},
@@ -13,14 +20,10 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 	// 	console.log(result)
 	// })
 
-	db.collection("Users").findOneAndUpdate(
-		{ _id: new ObjectID("5c52184eb9f45bb9fe9eeccb")},
-		{ $inc: {age: 1}},
-		{ returnOriginal: false} 
-	).then(result => {
-		console.log(result)
-	})
-
-	db.close();
-})
-
+	// db.collection("Users").findOneAndUpdate(
+	// 	{ _id: new ObjectID("5c52184eb9f45bb9fe9eeccb")},
+	// 	{ $inc: {age: 1}},
+	// 	{ returnOriginal: false} 
+	// ).then(result => {
+	// 	console.log(result)
+	// })
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+let {mongoose} = require("./db")
+let {User} = require("./models/user")
+let {Todo} = require("./models/todo")
+
+app.use(bodyParser.json())
+
+app.post("/todos", (req, res) => {
+	let todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save().then(doc => {
+		res.send(doc)
+	}, e => {
+		res.status(400).send(e)
+	})
+})
+
+
+app.listen(3000, () => {
+	console.log("server is online")
+})
+
